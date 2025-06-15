f58b2c1b2c02ec75b905dd8cb462c85368db8f41
gautamdev17 <gautamd.nxt@gmail.com>
2025-06-15 23:17:29 +0530

    Update lcdheader.h
    included floating point printing in lcd functionality

--- header_files/lcdheader.h
+++ header_files/lcdheader.h
@@ -1,5 +1,6 @@
 #include <reg51.h>
 #include <delayheader.h>
+#include <math.h>
 #define LCD_D P2 // 8 data pins connected to port 2(D0 to D7)
 sbit RS = P1^0;
 sbit RW = P1^1;
@@ -9,7 +10,8 @@ void lcd_init(void);
 void lcd_cmd(unsigned char);
 void lcd_data(unsigned char);
 void lcd_str(char *);
-
+void float_val(float ,int);
+void lcd_num(int n);
 
 void lcd_init(void)
 {
@@ -46,4 +48,44 @@ void lcd_str(char * s)//this function recieves the string as a char pointer
 	{
 		lcd_data(*s++);//pointer moves if not null
 	}
+}
+
+void float_val(float f,int p)// function to print floating point numbers with precision 'p'
+{
+	float temp;
+	int n;
+	n=f;
+	lcd_num(n);
+	lcd_data('.');
+	temp=(f-n)*pow(10,p);
+	n=temp;
+	lcd_num(n);
+	
+}
+
+
+
+void lcd_num(int n)
+{
+	int arr[5],i=0;
+	if(n==0)
+		lcd_data('0');
+	else
+	{
+		if(n<0)
+		{
+			lcd_data('-');
+			n=-n;
+		}
+	
+		while(n>0)
+		{
+			arr[i++]=n%10;
+			n=n/10;
+		}
+	
+		for(--i;i>=0;i--)
+			lcd_data(arr[i]+48);
+	}
+	
 }
