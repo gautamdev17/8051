6924c4246b7131ad2965029ff6b428ab36d44bf8
gautamdev17 <gautamd.nxt@gmail.com>
2025-06-22 10:44:36 +0530

    Update lcdheader.h

--- i2c/lcdheader.h
+++ i2c/lcdheader.h
@@ -1,81 +1,94 @@
-#include <reg51.h>
-#include <delayheader.h>
-#define LCD_D P2 // 8 data pins connected to port 2(D0 to D7)
-sbit RS = P1^0;
-sbit RW = P1^1;
-sbit E = P1^2;
+#include"delayheader.h"
+#define lcd_d P2
+sbit rs=P1^0;
+sbit rw=P1^1;
+sbit en=P1^2;
 
-void lcd_cmd(unsigned char cmd)
-{
-	LCD_D = cmd;// Assigning command to the data pins
-	RS = 0;// selects command register
-	RW = 0;// write operation
-	E = 1;// latch the data to the LCD
-	delay(2);// LCD is in internal operation mode
-	E = 0;// To re-latch the next byte
-}
+void lcd_init(void);
+void lcd_cmd(unsigned char);
+void lcd_data(unsigned char);
+void lcd_str(unsigned char *);
+//void float_val(float);
+//void lcd_num(int n);
+void delay(int);
+
 
 void lcd_init(void)
 {
-	lcd_cmd(0x01);//to clear display
-	lcd_cmd(0x02);//return cursor to home position
-	lcd_cmd(0x0c);//display on cursor off
-	lcd_cmd(0x38);//8 bit interfacing mode with both lines
-	lcd_cmd(0x80);//select the starting address of DDRAM
+	lcd_cmd(0x01);
+	lcd_cmd(0x02);
+	lcd_cmd(0x0c);
+	lcd_cmd(0x38);
+	lcd_cmd(0x80);
+	
 }
 
-void lcd_data(unsigned char d)
+
+void lcd_cmd(unsigned char c)
 {
-	LCD_D = d;// Assigning data to the data pins
-	RS = 1;// selects data register
-	RW = 0;// write operation
-	E = 1;// latch the data to the LCD
-	delay(2);// LCD is in internal operation mode
-	E = 0;// To re-latch the next byte
+	lcd_d=c;
+	rs=0;
+	rw=0;
+	en=1;
+	delay(2);
+	en=0;
+}
+	
+void lcd_data(unsigned char c)
+{
+	lcd_d=c;
+	rs=1;
+	rw=0;
+	en=1;
+	delay(2);
+	en=0;
+	
 }
 
-void lcd_str(char * s)//this function recieves the string as a char pointer
+void lcd_str(unsigned char *d)
 {
-	while(*s)//loop executes until null char is met
+	while(*d)
+		lcd_data(*d++);
+}
+
+/*void float_val(float f)
+{
+	float temp;
+	int n;
+	n=f;
+	lcd_num(n);
+	lcd_data('.');
+	temp=(f-n)*100;
+	n=temp;
+	lcd_num(n);
+	
+}
+
+
+
+void lcd_num(int n)
+{
+	int arr[5],i=0;
+	if(n==0)
+		lcd_data('0');
+	else
 	{
-		lcd_data(*s++);//pointer moves if not null
-	}
-}
-
-void lcd_int(int n)
-{
-	 int arr[5],i=0;
-	 
-	  
-	 if(n==0)
-   lcd_data('0');
-	 else
-	 { 
-		 if(n<0)
-		 {
-			 lcd_data('-');
-			 n=-n;
-		 } 
-				 
-		 while(n>0)
-		 {
-			 arr[i++] = n%10;
-			 n=n/10;		 
-		 }
-		 for(--i;i>=0;i--)
-			lcd_data(arr[i]+48);
+		if(n<0)
+		{
+			lcd_data('-');
+			n=-n;
 		}
-}
-
-void float_val(float f)
-{
-	 float temp;
-	  int n;
-	  n=f;
-	  lcd_int(n);
-	  lcd_data('.');
-	  temp=(f-n)*100;
-	  n=temp;
-   lcd_int(n);	
-	 
-}
+		
+		while(n>0)
+		{
+			arr[i++]=n%10;
+			n=n/10;
+		}
+		
+		for(--i;i>=0;i--)
+		lcd_data(arr[i]+48);
+		
+		
+	}
+	
+}*/
