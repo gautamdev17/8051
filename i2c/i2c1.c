c93213e7d9a2786782b08c675ad40bcab3a7c027
gautamdev17 <gautamd.nxt@gmail.com>
2025-06-22 10:44:20 +0530

    Update i2c1.c

--- i2c/i2c1.c
+++ i2c/i2c1.c
@@ -1,7 +1,8 @@
-#include <reg51.h>
-#include <lcdheader.h>
-#include <i2c.h>
-#include <delayheader.h>
+/*Write an ECP to write 'A' into EEPROM(AT24C08) and read 
+the data from EEPROM and display it on LCD using 8051*/
+#include<reg51.h>
+#include"lcdheader.h"
+#include "i2c.h"
 
 void main()
 {
@@ -9,11 +10,7 @@ void main()
 	lcd_str("i2c_EEPROM:");
 	delay(500);
 	i2c_slave_write(0xa0,0x00,'A');
-	//lcd_cmd(0x01);  // Clear
-	
-	//lcd_int(i2c_slave_read(0xA0, 0x00)); // Show ASCII value (like 65)
-	
 	lcd_data(i2c_slave_read(0xa0,0x00));
-	
 	while(1);
+	
 }
