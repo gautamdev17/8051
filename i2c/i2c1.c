2529477bd4cdd70ac2d1388439dfa83224213273
gautamdev17 <gautamd.nxt@gmail.com>
2025-06-22 09:51:32 +0530

    Update i2c1.c

--- i2c/i2c1.c
+++ i2c/i2c1.c
@@ -1,81 +1,19 @@
 #include <reg51.h>
+#include <lcdheader.h>
+#include <i2c.h>
 #include <delayheader.h>
-#define LCD_D P2 // 8 data pins connected to port 2(D0 to D7)
-sbit RS = P1^0;
-sbit RW = P1^1;
-sbit E = P1^2;
 
-void lcd_cmd(unsigned char cmd)
+void main()
 {
-	LCD_D = cmd;// Assigning command to the data pins
-	RS = 0;// selects command register
-	RW = 0;// write operation
-	E = 1;// latch the data to the LCD
-	delay(2);// LCD is in internal operation mode
-	E = 0;// To re-latch the next byte
-}
-
-void lcd_init(void)
-{
-	lcd_cmd(0x01);//to clear display
-	lcd_cmd(0x02);//return cursor to home position
-	lcd_cmd(0x0c);//display on cursor off
-	lcd_cmd(0x38);//8 bit interfacing mode with both lines
-	lcd_cmd(0x80);//select the starting address of DDRAM
-}
-
-void lcd_data(unsigned char d)
-{
-	LCD_D = d;// Assigning data to the data pins
-	RS = 1;// selects data register
-	RW = 0;// write operation
-	E = 1;// latch the data to the LCD
-	delay(2);// LCD is in internal operation mode
-	E = 0;// To re-latch the next byte
-}
-
-void lcd_str(char * s)//this function recieves the string as a char pointer
-{
-	while(*s)//loop executes until null char is met
-	{
-		lcd_data(*s++);//pointer moves if not null
-	}
-}
-
-void lcd_int(int n)
-{
-	 int arr[5],i=0;
-	 
-	  
-	 if(n==0)
-   lcd_data('0');
-	 else
-	 { 
-		 if(n<0)
-		 {
-			 lcd_data('-');
-			 n=-n;
-		 } 
-				 
-		 while(n>0)
-		 {
-			 arr[i++] = n%10;
-			 n=n/10;		 
-		 }
-		 for(--i;i>=0;i--)
-			lcd_data(arr[i]+48);
-		}
-}
-
-void float_val(float f)
-{
-	 float temp;
-	  int n;
-	  n=f;
-	  lcd_int(n);
-	  lcd_data('.');
-	  temp=(f-n)*100;
-	  n=temp;
-   lcd_int(n);	
-	 
+	lcd_init();
+	lcd_str("i2c_EEPROM:");
+	delay(500);
+	i2c_slave_write(0xa0,0x00,'A');
+	//lcd_cmd(0x01);  // Clear
+	
+	//lcd_int(i2c_slave_read(0xA0, 0x00)); // Show ASCII value (like 65)
+	
+	lcd_data(i2c_slave_read(0xa0,0x00));
+	
+	while(1);
 }
